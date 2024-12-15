a04518a5379889dd94d35b43de839e72cae6edab
juniorbotelho <junowerther@gmail.com>
2024-12-15 11:43:52 -0300

    feat: add global styles and improve page title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/globals.css'
+
 export default function RootLayout({
   children,
 }: {
@@ -5,6 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Airbnb | Locações por temporada, chalés, casas de praia e muito mais.</title>
+        <meta name="description" content="Reserve um Airbnb para cada tipo de viagem → 7 milhões de locações por temporada → 2 milhões de Preferidos dos Hóspedes → Mais de 220 países e regiões em todo o mundo." />
+      </head>
       <body>{children}</body>
     </html>
   )
