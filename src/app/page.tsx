c9f5e94f1c847a0676bb3e50133ac9fab474d2f1
juniorbotelho <junowerther@gmail.com>
2024-12-16 09:29:54 -0300

    feat: add label, name, and type props to Input component in search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Input from '@/components/Input'
 export default function Page() {
   return (
     <div className="p-2">
-      <Input>
-          <IconButton background="ui" iconName="search" title="Search" type="submit" />
+      <Input label="Inicie a sua busca" name="search" type="search">
+        <IconButton background="ui" iconName="search" title="Search" type="submit" />
       </Input>
     </div>
   )
