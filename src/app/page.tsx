49fbc21ce6b1838f0c6e504e90036451192930fb
juniorbotelho <junowerther@gmail.com>
2024-12-16 20:49:42 -0300

    feat: add image component to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import IconButton from '@/components/IconButton'
 import Input from '@/components/Input'
+import Image from 'next/image'
 
 export default function Page() {
   return (
@@ -7,6 +8,8 @@ export default function Page() {
       <Input label="Inicie a sua busca" name="search" type="search">
         <IconButton background="ui" iconName="search" title="Botão de pesquisa" type="submit" />
       </Input>
+
+      <Image width={1179} height={818} src="/img/assets/01/0001.jpg" alt="Casa segura" />
     </div>
   )
 }
