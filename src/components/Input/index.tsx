acb5fab8926ee3a3970733ba584516dd3b2e553f
juniorbotelho <junowerther@gmail.com>
2024-12-16 09:29:37 -0300

    feat: enhance Input component with label, name, and type props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,31 @@
-import { useId } from "react"
+import { type HTMLInputTypeAttribute, useId } from "react"
 
 type InputProps = {
   children: React.ReactNode | React.ReactNode[]
+  label: string
+  name: string
+  type: HTMLInputTypeAttribute
 }
 
-const Input: React.FC<InputProps> = ({ children }) => {
+const Input: React.FC<InputProps> = ({ children, label, name, type }) => {
   const id = useId()
 
   return (
-    <div
-      className="flex overflow-clip rounded-full ring-1 ring-gray-200 shadow-lg shadow-gray-200"
-    >
-      <label htmlFor={id} className="sr-only">Search</label>
-      <input id={id} className="w-full p-4 focus:outline-none" />
+    <div className="flex overflow-clip rounded-full ring-1 ring-gray-200 shadow-lg shadow-gray-200">
+      <label
+        className="sr-only"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+
+      <input
+        className="w-full p-4 focus:outline-none"
+        id={id}
+        name={name}
+        type={type}
+        placeholder={label}
+      />
 
       {children}
     </div>
