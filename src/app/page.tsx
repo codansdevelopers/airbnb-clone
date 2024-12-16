11b40c59932ccdb94f7fbcae2b86ebd42c30477c
juniorbotelho <junowerther@gmail.com>
2024-12-15 21:53:26 -0300

    feat: integrate Input and IconButton components into the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import IconButton from '@/components/IconButton'
+import Input from '@/components/Input'
+
 export default function Page() {
-  return <h1 className="text-xl">Hello, Next.js!</h1>
+  return (
+    <div className="p-2">
+      <Input>
+          <IconButton background="ui" iconName="search" title="Search" type="submit" />
+      </Input>
+    </div>
+  )
 }
