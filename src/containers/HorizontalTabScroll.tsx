66f030bda13d519e03e86aee45c1d7e9bca6ed31
juniorbotelho <junowerther@gmail.com>
2024-12-17 13:06:26 -0300

    feat: add HorizontalTabScroll component to fetch and display icons in tabs

---
+++ src/containers/HorizontalTabScroll.tsx
@@ -0,0 +1,35 @@
+import type { PublicApi } from '@/types/Api'
+import Tab from '@/components/Tab'
+
+const HorizontalTabScroll: React.FC = async () => {
+  // Aguarda a resposta da API.
+  // A função getPublicAssets é assíncrona, então o await é necessário.
+  const response = await getPublicAssets()
+
+  return response.icons.map((icon) => (
+    <Tab
+      key={icon.id}
+      title={icon.description}
+      image={{
+        source: icon.source,
+        description: icon.description,
+      }}
+    />
+  ))
+}
+
+const getPublicAssets = async (): Promise<PublicApi> => {
+  try {
+    const response = await fetch(`${process.env.HOSTNAME}/api.json`)
+    const api: PublicApi = await response.json()
+    // Retorna os dados da API.
+    return api
+  } catch (e) {
+    // Fazer tratamento de erro aqui ou enviar para algum serviço de monitoramento.
+    // Por enquanto, só vamos logar o erro no console.
+    console.error(e)
+    throw e
+  }
+}
+
+export default HorizontalTabScroll
