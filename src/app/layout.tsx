cffcdd8529c05afda60db70b5376043720b6ae69
juniorbotelho <junowerther@gmail.com>
2024-12-19 18:50:03 -0300

    refactor: remove padding class from body element in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         <title>Airbnb | Locações por temporada, chalés, casas de praia e muito mais.</title>
         <meta name="description" content="Reserve um Airbnb para cada tipo de viagem → 7 milhões de locações por temporada → 2 milhões de Preferidos dos Hóspedes → Mais de 220 países e regiões em todo o mundo." />
       </head>
-      <body className="p-10">
+      <body>
         {/* Cabeçalho do site */}
         <header>
           {header}
