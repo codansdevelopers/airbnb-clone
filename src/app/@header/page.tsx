9fe8988f43aed5d4e5b7b37e2dad88f51163fdbf
juniorbotelho <junowerther@gmail.com>
2024-12-19 16:26:43 -0300

    feat: add SearchForm component above HorizontalScrollTab in header page

--- src/app/@header/page.tsx
+++ src/app/@header/page.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from 'react'
 import HorizontalScrollTab from '@/widgets/HorizontalScrollTab'
+import SearchForm from '@/widgets/SearchForm'
 
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
   return (
-    <Suspense>
-      <HorizontalScrollTab />
-    </Suspense>
+    <>
+      <SearchForm />
+      <Suspense>
+        <HorizontalScrollTab />
+      </Suspense>
+    </>
   )
 }
