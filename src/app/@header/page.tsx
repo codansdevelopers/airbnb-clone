475626df37dfbbf45cd8c25eea3e1b130e07d43c
juniorbotelho <junowerther@gmail.com>
2024-12-19 20:55:14 -0300

    feat: add TopBar component to header for improved navigation

--- src/app/@header/page.tsx
+++ src/app/@header/page.tsx
@@ -1,14 +1,15 @@
 import { Suspense } from 'react'
 import HorizontalScrollTab from '@/widgets/HorizontalScrollTab'
 import SearchForm from '@/widgets/SearchForm'
+import TopBar from '@/widgets/TopBar'
 
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
   return (
     <>
+      <TopBar />
       <SearchForm />
-
       <Suspense>
         <HorizontalScrollTab />
       </Suspense>
