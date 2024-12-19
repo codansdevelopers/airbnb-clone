a117f1f93079648ea498930fdd1b4b134924016d
juniorbotelho <junowerther@gmail.com>
2024-12-19 16:34:14 -0300

    refactor: update RootLayout to add class names and restructure body content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,13 +14,16 @@ export default function RootLayout({
         <title>Airbnb | Locações por temporada, chalés, casas de praia e muito mais.</title>
         <meta name="description" content="Reserve um Airbnb para cada tipo de viagem → 7 milhões de locações por temporada → 2 milhões de Preferidos dos Hóspedes → Mais de 220 países e regiões em todo o mundo." />
       </head>
-      <body>
+      <body className="p-10">
+        {/* Cabeçalho do site */}
         <header>
           {header}
         </header>
-        <div className="p-2">
+
+        {/* Conteúdo principal */}
+        <main>
           {children}
-        </div>
+        </main>
       </body>
     </html>
   )
