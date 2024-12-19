fc3804c47b4019843e8685ccb8eece0ac4a10d8d
juniorbotelho <junowerther@gmail.com>
2024-12-19 10:37:44 -0300

    refactor: remove Suspense wrapper and unnecessary div from Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import { Suspense } from 'react'
-import HorizontalScrollTab from '@/widgets/HorizontalScrollTab'
 import SearchForm from '@/widgets/SearchForm'
 
 export default async function Page() {
   return (
-    <div className="p-2">
-      <SearchForm />
-      <Suspense>
-        <HorizontalScrollTab />
-      </Suspense>
-    </div>
+    <SearchForm />
   )
 }
