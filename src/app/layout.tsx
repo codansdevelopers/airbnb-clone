94dcb0ab8ec0afe6c7b035c8a447c94f915526a9
juniorbotelho <junowerther@gmail.com>
2024-12-19 16:11:26 -0300

    feat: integrate Montserrat font into RootLayout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/globals.css'
+import { montserrat } from '@/fonts/google'
 
 export default function RootLayout({
   children,
@@ -8,7 +9,7 @@ export default function RootLayout({
   categories: React.ReactNode,
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={montserrat.variable}>
       <head>
         <title>Airbnb | Locações por temporada, chalés, casas de praia e muito mais.</title>
         <meta name="description" content="Reserve um Airbnb para cada tipo de viagem → 7 milhões de locações por temporada → 2 milhões de Preferidos dos Hóspedes → Mais de 220 países e regiões em todo o mundo." />
