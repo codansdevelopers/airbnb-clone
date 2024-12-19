aeea7bb18fbb83afcc1865b410ca1bc7bc8e4861
juniorbotelho <junowerther@gmail.com>
2024-12-19 13:14:29 -0300

    refactor: update SearchForm to use Image component for icon in IconButton

--- src/widgets/SearchForm.tsx
+++ src/widgets/SearchForm.tsx
@@ -1,10 +1,24 @@
+import Image from 'next/image'
 import IconButton from '@/components/IconButton'
 import Input from '@/components/Input'
 
 const SearchForm: React.FC = (): React.ReactNode => {
   return (
     <Input label="Inicie a sua busca" name="search" type="search">
-      <IconButton background="ui" iconName="search" title="Botão de pesquisa" type="submit" />
+      <IconButton
+        background="red"
+        title="Botão de pesquisa"
+        type="submit"
+        icon={(
+          <Image
+            className="invert"
+            src="/assets/search.png"
+            alt="Lupa"
+            width={24}
+            height={24}
+          />
+        )}
+      />
     </Input>
   )
 }
