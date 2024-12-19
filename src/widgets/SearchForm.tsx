bd963fc233503c3a1204b640af77d4850c27d7c3
juniorbotelho <junowerther@gmail.com>
2024-12-19 19:02:44 -0300

    feat: wrap SearchForm component with Boundary for layout containment

--- src/widgets/SearchForm.tsx
+++ src/widgets/SearchForm.tsx
@@ -1,27 +1,31 @@
 import Image from 'next/image'
+
+import Boundary from '@/components/Boundary'
 import IconButton from '@/components/IconButton'
 import Input from '@/components/Input'
 
 const SearchForm: React.FC = (): React.ReactNode => {
   return (
-    <div className="max-w-2xl mx-auto">
-      <Input label="Inicie a sua busca" name="search" type="search">
-        <IconButton
-          background="red"
-          title="Botão de pesquisa"
-          type="submit"
-          icon={(
-            <Image
-              className="invert"
-              src="/assets/search.png"
-              alt="Lupa"
-              width={24}
-              height={24}
-            />
-          )}
-        />
-      </Input>
-    </div>
+    <Boundary direction="top-sides">
+      <div className="max-w-2xl mx-auto">
+        <Input label="Inicie a sua busca" name="search" type="search">
+          <IconButton
+            background="red"
+            title="Botão de pesquisa"
+            type="submit"
+            icon={(
+              <Image
+                className="invert"
+                src="/assets/search.png"
+                alt="Lupa"
+                width={24}
+                height={24}
+              />
+            )}
+          />
+        </Input>
+      </div>
+    </Boundary>
   )
 }
 
