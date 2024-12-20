2a4aaf5f5d3ea0af4cb5d2b350ba2d0995aa35ae
juniorbotelho <junowerther@gmail.com>
2024-12-20 14:54:25 -0300

    feat: add children prop to IconButton for flexible content rendering

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -3,6 +3,7 @@ import cx from 'clsx'
 type Props = {
   background: 'transparent' | 'red'
   border?: boolean
+  children?: React.ReactNode
   icon: React.ReactNode
   title: string
   type: 'button' | 'submit' | 'reset'
@@ -11,6 +12,7 @@ type Props = {
 const IconButton: React.FC<Props> = ({
   background,
   border,
+  children,
   icon,
   title,
   type,
@@ -19,23 +21,51 @@ const IconButton: React.FC<Props> = ({
     <button
       title={title}
       type={type}
-      className={cx("m-2 p-2 rounded-full", getBackground(background), getBorder(border))}
+      className={cx(
+        getRound(children),
+        getBackground(background),
+        getBorder(border),
+      )}
     >
-      <span>{icon}</span>
+      <div className={getFlexibleLayout(children)}>
+        <span className="opacity-80">
+          {icon}
+        </span>
+
+        {/* Se o elemento possuir um filho, então renderiza o `span`. */}
+        {children && (
+          <span className="text-xs font-medium">
+            {children}
+          </span>
+        )}
+      </div>
     </button>
   )
+}
+
+const getRound = (children?: React.ReactNode): string => {
+  return cx({
+    'm-2 py-3 px-4 rounded-xl': children,
+    'm-2 p-2 rounded-full'    : !children,
+  })
 }
 
 const getBackground = (background: string): string => {
   return cx({
     'bg-transparent': background === 'transparent',
-    'bg-red-500':     background === 'red',
+    'bg-red-500'    :     background === 'red',
   })
 }
 
 const getBorder = (border?: boolean): string => {
   return cx({
     'border border-gray-400': border,
+  })
+}
+
+const getFlexibleLayout = (children?: React.ReactNode): string => {
+  return cx({
+    'flex justify-center items-center': children,
   })
 }
 
