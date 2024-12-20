5030150f1157071c60c84be2621399b009cb2341
juniorbotelho <junowerther@gmail.com>
2024-12-19 21:24:36 -0300

    feat: enhance TopBar component with navigation links and highlight functionality

--- src/widgets/TopBar.tsx
+++ src/widgets/TopBar.tsx
@@ -1,14 +1,45 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import cx from 'clsx'
+
 import Boundary from '@/components/Boundary'
 import Logo from '@/components/Logo'
 
 const TopBar: React.FC = () => {
+  const pathname = usePathname()
+
   return (
     <Boundary direction="top-sides">
-      <div>
+      <div className="flex justify-between items-center">
         <Logo />
+
+        <div className="flex gap-4">
+          <Link href="/">
+            <span className={highlightText(pathname, '/')}>
+              Acomodações
+            </span>
+          </Link>
+          <Link href="/experiences">
+            <span className={highlightText(pathname, '/experiences')}>
+              Experiências
+            </span>
+          </Link>
+        </div>
+
+        <div>
+          3th Item
+        </div>
       </div>
     </Boundary>
   )
+}
+
+const highlightText = (pathname: string, target: string) => {
+  return cx({
+    'font-semibold': pathname.startsWith(target),
+  })
 }
 
 export default TopBar
