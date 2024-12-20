93f1ee13cbb70269e119dc89e138daa4c076c3d9
juniorbotelho <junowerther@gmail.com>
2024-12-19 21:24:30 -0300

    refactor: remove unnecessary comments from layout component for cleaner code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,9 @@ export default function RootLayout({
         <meta name="description" content="Reserve um Airbnb para cada tipo de viagem → 7 milhões de locações por temporada → 2 milhões de Preferidos dos Hóspedes → Mais de 220 países e regiões em todo o mundo." />
       </head>
       <body>
-        {/* Cabeçalho do site */}
         <header>
           {header}
         </header>
-
-        {/* Conteúdo principal */}
         <main>
           {children}
         </main>
