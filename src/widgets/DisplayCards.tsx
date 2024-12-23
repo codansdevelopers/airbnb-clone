43b5f44fa69107a745c361e163769ed788dfd6e3
juniorbotelho <junowerther@gmail.com>
2024-12-23 09:52:16 -0300

    feat: add DisplayCards component to render accommodation cards

---
+++ src/widgets/DisplayCards.tsx
@@ -0,0 +1,12 @@
+import Boundary from '@/components/Boundary'
+import Card from '@/components/Card'
+
+const DisplayCards: React.FC = () => {
+  return (
+    <Boundary direction="top-sides">
+      <Card />
+    </Boundary>
+  )
+}
+
+export default DisplayCards
