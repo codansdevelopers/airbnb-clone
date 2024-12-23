2ef625c52537cdb60d5399a032a2d759607a8db2
juniorbotelho <junowerther@gmail.com>
2024-12-23 10:42:24 -0300

    feat: update DisplayCards component to render a grid of Card components with images

--- src/widgets/DisplayCards.tsx
+++ src/widgets/DisplayCards.tsx
@@ -1,11 +1,34 @@
+import Image from 'next/image'
+
 import Boundary from '@/components/Boundary'
 import Card from '@/components/Card'
 
+const tweentyCount = new Array(20).fill(null)
+
 const DisplayCards: React.FC = () => {
   return (
-    <Boundary direction="top-sides">
-      <Card />
-    </Boundary>
+    <section>
+      <Boundary direction="top-sides">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          {tweentyCount.map(() => (
+            <Card
+              title="Colônia de Pescadores, Brasil"
+              host="Maria"
+              duration="8 - 13 de jan"
+              price={229}
+            >
+              <Image
+                className="border-none object-cover aspect-square"
+                width={1180}
+                height={820}
+                src="/img/photos/01/0001.jpg"
+                alt=""
+              />
+            </Card>
+          ))}
+        </div>
+      </Boundary>
+    </section>
   )
 }
 
