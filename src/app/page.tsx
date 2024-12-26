85dd39d87e87cce94dc6531e59ed474981f369e8
juniorbotelho <junowerther@gmail.com>
2024-12-26 18:49:44 -0300

    refactor: remove Suspense wrapper from Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Suspense } from "react"
-
 import DisplayCards from "@/widgets/DisplayCards"
 
 export default async function Page() {
   return (
-    <Suspense>
-      <DisplayCards />
-    </Suspense>
+    <DisplayCards />
   )
 }
