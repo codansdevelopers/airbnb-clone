d4bcacee12df7b68293c593cdee2df34836c5ddd
juniorbotelho <junowerther@gmail.com>
2025-01-02 12:04:59 -0300

    style: change background color of FooterLayout from gray-200 to gray-100

--- src/widgets/FooterLayout.tsx
+++ src/widgets/FooterLayout.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/Footer'
 
 const FooterLayout = (): React.JSX.Element => {
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-100">
       <Boundary direction="around">
         <Footer />
       </Boundary>
