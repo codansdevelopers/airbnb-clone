0120b1ee34f7e8d5bee43616414e8fc4be5343cd
juniorbotelho <junowerther@gmail.com>
2025-01-02 12:04:53 -0300

    style: update Footer component to improve list item layout and styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,11 +6,11 @@ const Footer = (): React.JSX.Element => {
     <div className="py-4">
       <h4>&copy; {year} Airbnb, Inc.</h4>
 
-      <ul>
-        <li>Privacidade</li>
-        <li>Termos</li>
-        <li>Mapa do site</li>
-        <li>Informações da empresa</li>
+      <ul className="text-sm mt-2">
+        <li className="inline mr-1.5">Privacidade |</li>
+        <li className="inline mr-1.5">Termos |</li>
+        <li className="inline mr-1.5">Mapa do site |</li>
+        <li className="inline mr-1.5">Informações da empresa</li>
       </ul>
     </div>
   )
