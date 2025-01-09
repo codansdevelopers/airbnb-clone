29ae1f097856c86e5c03717f983a49292d259349
codansdevelopers <developers@codans.com.br>
2025-01-09 16:48:52 -0300

    feat: integrate isDynamicPath utility in Header component for dynamic path handling

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { usePathname } from "next/navigation"
+import { isDynamicPath } from '@/utils/isDynamicPath'
 
 import SearchForm from "@/widgets/SearchForm"
 import TopBar from "@/widgets/TopBar"
@@ -8,19 +9,14 @@ import TopBar from "@/widgets/TopBar"
 const Header = (): React.JSX.Element => {
   const pathname = usePathname()
 
-  // Checa se a rota é um subpath dinâmico, como `/qualquer-coisa-slug`, se for, não exibe alguns itens.
-  // [\w] - Verifica se a rota começa com qualquer caractere alfanumérico.
-  // (.*) - Verifica se a rota tem qualquer coisa depois do caractere alfanumérico.
-  const isDynamicPath = /[\w](.*)/.test(pathname)
-
   return (
     <header>
-      <TopBar isDynamicPath={isDynamicPath}>
+      <TopBar isDynamicPath={isDynamicPath(pathname)}>
         <SearchForm compact={true} />
       </TopBar>
 
       {/* Não renderiza o SearchForm de qualquer jeito em subpaths */}
-      {!isDynamicPath && (
+      {!isDynamicPath(pathname) && (
         <SearchForm />
       )}
     </header>
