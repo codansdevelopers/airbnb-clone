8788f350b952088266dd871d1f4e3804e5e4ab10
codansdevelopers <developers@codans.com.br>
2025-01-10 08:14:13 -0300

    feat: enhance AccommodationDescription component with additional features and improved styling

--- src/widgets/AccomodationDescription.tsx
+++ src/widgets/AccomodationDescription.tsx
@@ -1,16 +1,21 @@
-import { IconStarFilled } from '@tabler/icons-react'
+import { IconStarFilled, IconPhoto, IconToolsKitchen2, IconDesk, IconPool, IconBrandCarbon, IconSailboat, IconWifi, IconParking, IconAlarmSmoke, IconDog } from '@tabler/icons-react'
+import type { Accommodation } from '@/types/Api'
 
 import Boundary from '@/components/Boundary'
 
-const AccommodationDescription = (): React.JSX.Element => {
+type AccommodationDescriptionProps = {
+  post: Accommodation
+}
+
+const AccommodationDescription = ({ post }: AccommodationDescriptionProps): React.JSX.Element => {
   return (
-    <Boundary direction="around">
-      <div arial-label="Descrição">
-        <h2 className="font-medium sm:text-xl">
+    <Boundary direction="top-sides">
+      <div className="mt-2">
+        <h2 className="font-semibold sm:text-2xl">
           Espaço inteiro: casa de campo em Minas Gerais
         </h2>
 
-        <ul className="flex gap-2 text-sm sm:text-base">
+        <ul className="flex gap-2 text-sm sm:text-lg">
           <li>10 hóspedes</li>
           <li>5 quartos</li>
           <li>5 camas</li>
@@ -21,6 +26,64 @@ const AccommodationDescription = (): React.JSX.Element => {
           <IconStarFilled className="size-4" />
           <span className="text-sm">4.9 (20 avaliações)</span>
         </span>
+      </div>
+
+      <div className="mt-10">
+        <h2 className="text-xl font-semibold">
+          O que esse lugar oferece?
+        </h2>
+
+        <ul className="py-6 items-center grid grid-rows-5 grid-flow-col gap-y-6">
+          <li className="flex gap-1.5">
+            <IconPhoto />
+            <span>Vista para as montanhas</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconToolsKitchen2 />
+            <span>Cozinha</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconDesk />
+            <span>Espaço de tabalho exclusivo</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconPool />
+            <span>Piscina privativa - disponível o ano todo</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconBrandCarbon />
+            <span className="line-through">Alarme de monóxido de carbono</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconSailboat />
+            <span>Vista para as águas</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconWifi />
+            <span>Wi-Fi rápido (83 Mbps)</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconParking />
+            <span>Estacionamento incluído</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconDog />
+            <span>Permitido animais</span>
+          </li>
+
+          <li className="flex gap-1.5">
+            <IconAlarmSmoke />
+            <span className="line-through">Detector de fumaça</span>
+          </li>
+        </ul>
       </div>
     </Boundary>
   )
