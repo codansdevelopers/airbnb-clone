4d007299ffec7faa189b80cdd982f588f4124e80
juniorbotelho <junowerther@gmail.com>
2025-01-12 13:28:40 -0300

    refactor: update FooterContent layout for improved spacing and link formatting

--- src/widgets/FooterContent.tsx
+++ src/widgets/FooterContent.tsx
@@ -5,16 +5,33 @@ const FooterContent = (): React.JSX.Element => {
   const year = new Date().getFullYear()
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 mt-8">
       <Boundary direction="around">
         <div className="py-4">
           <h4>&copy; {year} Airbnb, Inc.</h4>
 
           <ul className="text-sm mt-2">
-            <li className="inline mr-1.5">Privacidade |</li>
-            <li className="inline mr-1.5">Termos |</li>
-            <li className="inline mr-1.5">Mapa do site |</li>
-            <li className="inline mr-1.5">Informações da empresa</li>
+            <li className="inline-block">
+              Privacidade
+            </li>
+            <li className="inline-block">
+              <span className="mx-1.5">
+                &middot;
+              </span>
+              Termos
+            </li>
+            <li className="inline-block">
+              <span className="mx-1.5">
+                &middot;
+              </span>
+              Mapa do site
+            </li>
+            <li className="inline-block">
+              <span className="mx-1.5">
+                &middot;
+              </span>
+              Informações da empresa
+            </li>
           </ul>
         </div>
       </Boundary>
